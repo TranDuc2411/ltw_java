de992685714f6ef0e5501ea082208c30cea4abd9
01tranduc <tranngocminhduc2411@gmail.com>
2023-07-31 17:09:24 +0700

    fix dao to dbhandle

--- WebApplication1/src/java/controller/loginController.java
+++ WebApplication1/src/java/controller/loginController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import dbhanddle.AccountApiDao;
+import dbhandle.AccountApiDao;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
--- WebApplication1/src/java/dbhandle/AccountApiDao.java
+++ WebApplication1/src/java/dbhandle/AccountApiDao.java
@@ -1,4 +1,4 @@
-package dbhanddle;
+package dbhandle;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
